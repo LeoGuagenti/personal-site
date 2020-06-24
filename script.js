deb4ef9a38336b671a9f7b3ecb85404362b54d9b
LeoGuagenti <davinci5100@hotmail.com>
2020-06-24 19:52:26 -0400

    small layout and file structure changes

--- script.js
+++ script.js
@@ -18,7 +18,7 @@ var menu        = document.getElementById('menu');
 var mobileMenu  = document.getElementById('mobile-menu-links');
 
 //menu nav listeners
-document.getElementById('m1').addEventListener("click", function(){scroll(home), false});
+document.getElementById('m1').addEventListener("click", function(){toggleMenu();scroll(home), false});
 document.getElementById('m2').addEventListener("click", function(){scroll(about), false});
 document.getElementById('m3').addEventListener("click", function(){scroll(projects), false});
 document.getElementById('m4').addEventListener("click", function(){scroll(contact), false});
@@ -29,10 +29,10 @@ document.getElementById('m7').addEventListener("click", function(){scroll(about)
 document.getElementById('m8').addEventListener("click", function(){scroll(projects), false});
 document.getElementById('m9').addEventListener("click", function(){scroll(contact), false});
 
-document.getElementById('m10').addEventListener("click", function(){scroll(home), false});
-document.getElementById('m11').addEventListener("click", function(){scroll(about), false});
-document.getElementById('m12').addEventListener("click", function(){scroll(projects), false});
-document.getElementById('m13').addEventListener("click", function(){scroll(contact), false});
+document.getElementById('m10').addEventListener("click", function(){toggleMenu(); scroll(home), false});
+document.getElementById('m11').addEventListener("click", function(){toggleMenu(); scroll(about), false});
+document.getElementById('m12').addEventListener("click", function(){toggleMenu(); scroll(projects), false});
+document.getElementById('m13').addEventListener("click", function(){toggleMenu(); scroll(contact), false});
 
 //button nav listeners
 document.getElementById('b1').addEventListener("click", function(){scroll(projects), false});
@@ -42,7 +42,7 @@ document.getElementById('b4').addEventListener("click", function(){scroll(home),
 
 //link listeners
 document.getElementById('l1').addEventListener("click", function(){window.location = "https://github.com/LeoGuagenti/url-shortener", false});
-document.getElementById('l2').addEventListener("click", function(){window.location = "https://github.com/LeoGuagenti/web-crawler", false});
+document.getElementById('l2').addEventListener("click", function(){window.location = "https://github.com/LeoGuagenti/web-crawler-v2", false});
 document.getElementById('l3').addEventListener("click", function(){window.location = "https://github.com/LeoGuagenti/Natural-Selection-Simulation", false});
 document.getElementById('l4').addEventListener("click", function(){window.location = "https://github.com/LeoGuagenti", false});
 document.getElementById('l5').addEventListener("click", function(){window.location = "https://github.com/LeoGuagenti", false});
